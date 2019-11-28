fd4a9b46dd9b0072b2e23aa6a4d6f3185010dd1d
wadhwas <shailesh.wadhwa@hmhco.com>
2019-11-28 17:45:50 +0000

    PROBLEM SOLVED

--- excercise-08/http-collect.js
+++ excercise-08/http-collect.js
@@ -1,5 +1,29 @@
 const http = require('http');
 
+const bl = require('bl');
 http.get(process.argv[2], function callback(response){
-    response.pipe()
+    response.pipe(bl(function(error, data){
+        console.log(data.length);
+        console.log(data.toString());
+    }));
 });
+
+/*
+OFFICIAL SOLUTION
+
+    'use strict'
+    const http = require('http')
+    const bl = require('bl')
+    
+    http.get(process.argv[2], function (response) {
+      response.pipe(bl(function (err, data) {
+        if (err) {
+          return console.error(err)
+        }
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+      }))
+    })
+
+*/
