0c3ca965d9dac138b6f739afd6a3fd70334a6ef4
wadhwas <shailesh.wadhwa@hmhco.com>
2019-11-28 15:21:49 +0000

    excercise-06 completed

--- excercise-06/make-it-modular.js
+++ excercise-06/make-it-modular.js
@@ -1,13 +1,33 @@
-const add = require("./module")
+const mymodule = require("./mymodule")
 
 const folder = process.argv[2]
-const ext = '.' + process.argv[3]
+const ext = process.argv[3]
 
-    add(folder,ext, function(err, result) {
+mymodule(folder, ext, function(err, arr) {
       if(err) { // Best practice to handle your errors
           console.log(err)
       } else { // Implement the logic, what you want to do once you recieve the response back 
-        console.log(result) 
+        for (var i = 0; i < arr.length; i++) {
+          console.log(arr[i]);
+        }
       }
     })
 
+/*
+  OFFICIAL SOLUTION
+  
+    'use strict'
+    const filterFn = require('./solution_filter.js')
+    const dir = process.argv[2]
+    const filterStr = process.argv[3]
+    
+    filterFn(dir, filterStr, function (err, list) {
+      if (err) {
+        return console.error('There was an error:', err)
+      }
+    
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+*/
